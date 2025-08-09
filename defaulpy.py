3d808e4db90611d8a51114fd604b1b49e9efd3e7
GMTKTEK <billar52@hotmail.com>
2025-08-09 17:32:14 +0100

    Update defaulpy.py

--- defaulpy.py
+++ defaulpy.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 # Módulo principal para el addon.
 
@@ -14,10 +15,39 @@ plugin = routing.Plugin()
 
 @plugin.route('/')
 def index():
-    xbmcgui.ListItem("Peliculas | Movies")
-    xbmcgui.ListItem("Series | TV Shows")
-    xbmcgui.ListItem("Documentales | Documentaries")
-    xbmcgui.ListItem("Cocina | Cooking")
+    # Menú principal del addon
+    addon.add_directory({
+        'mode': 'peliculas',
+        'url': '',
+        'title': 'Peliculas | Movies',
+        'is_folder': True,
+    })
+    addon.add_directory({
+        'mode': 'series',
+        'url': '',
+        'title': 'Series | TV Shows',
+        'is_folder': True,
+    })
+    addon.add_directory({
+        'mode': 'documentales',
+        'url': '',
+        'title': 'Documentales | Documentaries',
+        'is_folder': True,
+    })
+    addon.add_directory({
+        'mode': 'cocina',
+        'url': '',
+        'title': 'Cocina | Cooking',
+        'is_folder': True,
+    })
+
+
+@plugin.route('/peliculas')
+def peliculas():
+    # Aquí puedes añadir los ítems de tu lista de películas
+    xbmcgui.ListItem("El Señor de los Anillos: La Comunidad del Anillo")
+    xbmcgui.ListItem("El Señor de los Anillos: Las Dos Torres")
+
 
 if __name__ == '__main__':
     plugin.run()
